9a8bba0471d9fbc22faf174c5a3929fd47f143d8
Tak943 <takf05121@gamil.com>
2026-07-06 16:55:40 +0900

    add: file-converter

---
+++ file-converter.py
@@ -0,0 +1,24 @@
+import markdown
+import sys
+
+if len(sys.argv) != 4:
+    print("正しい引数を入力してください")
+    print("引数の形:")
+    print("python3 file-converter.py markdown 入力ファイル 出力先ファイル")
+    sys.exit(1)
+
+inputpath = sys.argv[2]
+outputpath = sys.argv[3]
+
+with open(inputpath, "r", encoding="utf-8") as f:
+    md_text = ""
+    while True:
+        line = f.readline()
+        if line == "":
+            break
+        md_text += line
+    # md_text = f.read()
+
+with open(outputpath, "w", encoding="utf-8") as f:
+    md = markdown.Markdown()
+    f.write(md.convert(md_text))
